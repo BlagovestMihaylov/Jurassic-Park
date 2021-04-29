8100400233e4c3fb36cd29dc97182feafbde70e6
BlagovestMihaylov <BlagovestMihaylov@abv.bg>
2021-04-30 02:35:00 +0300

    added headers to the main file

---
+++ source/Park.cpp
@@ -0,0 +1,12 @@
+#include "../include/SmallCage.hpp"
+#include "../include/MediumCage.hpp"
+#include "../include/LargeCage.hpp"
+#include "../include/Dinosaur.hpp"
+#include "../include/String.hpp"
+#include "../include/Vector.hpp"
+
+int main()
+{
+
+    return 0;
+}
